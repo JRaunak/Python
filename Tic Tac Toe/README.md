c5449520da17bb7e2c80eafac5b49f69b98feb23
Raunak Jain <66216637+JRaunak@users.noreply.github.com>
2020-10-15 00:18:16 +0530

    Add files via upload

---
+++ Tic Tac Toe/README.md
@@ -0,0 +1,33 @@
+# Additional Modules
+- >  ***pygame***
+    - A graphics module for python
+    - To download using command line, type "pip install pygame" in cmd without braces
+    - The PyPI page for the module is [pygame.PyPI](https://pypi.org/project/pygame/)
+
+- >  ***PyInstaller*** (optional)
+    - A module to compile python scripts into standalone binaries/executables(.exe)
+    - To use it, type "pyinstaller \<flags> \<filename>" in cmd in your python script directory
+    - Flags are optional to use
+    - Here are some basic Flags:
+        - -w : To turn off the terminal while running the script
+        - --onefile : To compile the whole script and dependent scripts in a single binary file
+        - --icon <icon.ico> : To add an icon to the compiled binary
+    - To download it using command line, type "pip install PyInstaller"
+    - The PyPI page for the module is [pyinstaller.PyPI](https://pypi.org/project/PyInstaller/)
+
+# Structure of Code
+- The directory has 1 python script containing the entire code.
+- There is also an exe file with same name as the python script.
+- A Dependency named 'Winnings.dat' consists of some serialized data for the script
+
+# About
+- Running the .exe file doesn't require any download or even python installed<br>It is a standalone program.
+- The .exe file is for those who just want to play the game.
+- Those who want to check the source code, or make changes in it shall use the python script.
+- To run the python script, all the non-optional modules/libraries are required.
+- The PyInstaller module is required only for those who want <br> to rebuild their own script into an executable.
+
+# Misc
+- This is a basic game made using python's pygame module & compiled using PyInstaller
+- For information on pygame, refer to [Pygame Docs](https://www.pygame.org/docs/)
+- For additional information on PyInstaller, refer to [PyInstaller Docs](https://pyinstaller.readthedocs.io/en/stable/)
---
+++ Tic Tac Toe/TicTacToe.py
@@ -0,0 +1,147 @@
+from pickle import load
+import numpy as np
+import pygame
+from pygame import mouse
+
+pygame.init()
+screen = pygame.display.set_mode((606, 606))
+pygame.display.set_caption('Tic Tac Toe')
+input()
+Winning = load(open('Winnings.dat', 'rb'))
+AllZero = np.zeros([3, 3])
+
+def Grid():
+    pygame.draw.line(screen, (70, 70, 210), (200, 0), (200, 606), 3)
+    pygame.draw.line(screen, (70, 70, 210), (403, 0), (403, 606), 3)
+
+    pygame.draw.line(screen, (70, 70, 210), (0, 200), (606, 200), 3)
+    pygame.draw.line(screen, (70, 70, 210), (0, 403), (606, 403), 3)
+
+
+def GetPos():
+    return mouse.get_pos()[0]//203, mouse.get_pos()[1]//203
+
+
+class Symbol():
+    def __init__(self):
+        self.click = False
+        self.x = -250
+        self.y = -250
+        self.pos = (-2, -2)
+        self.size = 90
+
+    def SetPos(self):
+        self.pos = mouse.get_pos()[0]//203, mouse.get_pos()[1]//203
+        self.x = 203*self.pos[0]+100
+        self.y = 203*self.pos[1]+100
+
+
+class Zero(Symbol):
+    sign = 'O'
+
+    def Draw(self):
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.size, 8)
+
+
+class Cross(Symbol):
+    sign = 'X'
+
+    def Draw(self):
+        x11, y11 = self.x-(self.size-5), self.y-(self.size-5)
+        x12, y12 = self.x+(self.size-5), self.y+(self.size-5)
+        x21, y21 = self.x+(self.size-5), self.y-(self.size-5)
+        x22, y22 = self.x-(self.size-5), self.y+(self.size-5)
+
+        pygame.draw.line(screen, (255, 255, 255), (x11, y11), (x12, y12), 12)
+        pygame.draw.line(screen, (255, 255, 255), (x21, y21), (x22, y22), 12)
+
+
+def Pointer(symbol):
+    x = mouse.get_pos()[0]
+    y = mouse.get_pos()[1]
+    if symbol == 'O':
+        pygame.draw.circle(screen, (0, 255, 180), (x, y), 20, 4)
+    elif symbol == 'X':
+        x11, y11 = x-15, y-15
+        x12, y12 = x+15, y+15
+        x21, y21 = x+15, y-15
+        x22, y22 = x-15, y+15
+
+        pygame.draw.line(screen, (0, 255, 180), (x11, y11), (x12, y12), 7)
+        pygame.draw.line(screen, (0, 255, 180), (x21, y21), (x22, y22), 7)
+
+
+def GridState(List):
+    state = AllZero
+    for sym in List:
+        if sym.click:
+            if sym.sign == 'X':
+                state[sym.pos] = 1
+            elif sym.sign == 'O':
+                state[sym.pos] = -1
+    return state.T
+
+
+def Winner(I):
+    match = np.array(np.where(I == 1))
+    match = np.array([match[:, 0], match[:, -1]])
+    yi, xi = tuple(match[0])
+    yf, xf = tuple(match[-1])
+    Xi, Yi = 100+203*xi, 100+203*yi
+    Xf, Yf = 100+203*xf, 100+203*yf
+    pygame.draw.line(screen, (0, 255, 180), (Xi, Yi), (Xf, Yf), 20)
+
+
+OX = [Zero()]
+for _ in range(4):
+    OX += [Cross(), Zero()]
+
+i = 0
+lim = 9
+W = AllZero
+Moves = []
+
+running = True
+while running:
+    screen.fill((128, 128, 255))
+    Grid()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if i != 9 and GetPos() not in Moves:
+                OX[i].click = True
+                OX[i].SetPos()
+                i += 1
+                Moves.append(GetPos())
+
+        elif event.type == pygame.MOUSEBUTTONUP and 5 <= i <= lim:
+            if i == 9:
+                lim = 0
+            for A in Winning:
+                if np.all(A*GridState(OX) == A):
+                    W = A
+                    i = 9
+                    break
+                elif np.all(A*GridState(OX) == -A):
+                    W = A
+                    i = 9
+                    break
+
+    for o_x in OX:
+        o_x.Draw()
+
+    if not np.all(W == AllZero):
+        Winner(W)
+
+    try:
+        if OX[i].sign == 'X':
+            Pointer('X')
+        elif OX[i].sign == 'O':
+            Pointer('O')
+    except:
+        pass
+
+    pygame.display.flip()
